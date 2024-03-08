c15b950e800cb98dc6c59570c32917b268542d74
h672645 <111350084+h672645@users.noreply.github.com>
2024-03-08 02:12:49 +0100

    Oppdatert

--- SpaceObjectsForms/Form1.cs
+++ SpaceObjectsForms/Form1.cs
@@ -16,5 +16,10 @@ namespace SpaceObjectsForms
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
